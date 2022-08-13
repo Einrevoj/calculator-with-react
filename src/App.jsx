a5d11ecba1c02f3de94d70faebd9ae0088ee6d00
Einrevoj <jovcano28@gmail.com>
2022-08-13 08:21:11 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import(useState);
+
+function App() {
+  return (
+    <div className="container">
+      <h1>hello world</h1>
+    </div>
+  );
+}
+
+export default App;
