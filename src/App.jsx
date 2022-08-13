a917763a684caf9bf1cddcb9859b88ff69e48c19
Einrevoj <jovcano28@gmail.com>
2022-08-13 09:21:33 +0800

    updated html files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,197 @@
 import "./App.css";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
 import { useState } from "react";
 import(useState);
 
 function App() {
   return (
-    <div className="container">
-      <h1>hello world</h1>
+    <div className="d-flex align-items-center justify-content-center mx-auto vh-100 w-100">
+      <div id="calculator" className="py-2 rounded">
+        <div className="container">
+          <div className="row g-2">
+            <div className="col-12">
+              <h1 id="display" className="fs-1 my-4 text-end text-white">
+                0
+              </h1>
+            </div>
+            <div className="col-3">
+              <div
+                className="button
+                            fs-5 fw-bold text-bg-secondary text-center rounded-circle"
+                id="clearMemoryButton"
+              >
+                C
+              </div>
+            </div>
+            <div className="col-3">
+              <div
+                className="button
+                            fs-5 fw-bold text-bg-secondary text-center rounded-circle"
+                onclick="switchSign()"
+              >
+                ±
+              </div>
+            </div>
+            <div className="col-3">
+              <div
+                className="button
+                            fs-5 fw-bold text-bg-secondary text-center rounded-circle"
+                onclick="switchToPercent()"
+              >
+                %
+              </div>
+            </div>
+            <div className="col-3">
+              <div
+                className="button
+                            fs-5 fw-bold text-bg-warning text-center rounded-circle"
+                onclick="clickOperation('/')"
+              >
+                ÷
+              </div>
+            </div>
+
+            <div className="col-3">
+              <div
+                className="button
+                            fs-5 fw-bold text-bg-dark text-center rounded-circle"
+                onclick="clickNumber(7)"
+              >
+                7
+              </div>
+            </div>
+            <div className="col-3">
+              <div
+                className="button
+                            fs-5 fw-bold text-bg-dark text-center rounded-circle"
+                onclick="clickNumber(8)"
+              >
+                8
+              </div>
+            </div>
+            <div className="col-3">
+              <div
+                className="button
+                            fs-5 fw-bold text-bg-dark text-center rounded-circle"
+                onclick="clickNumber(9)"
+              >
+                9
+              </div>
+            </div>
+            <div className="col-3">
+              <div
+                className="button
+                            fs-5 fw-bold text-bg-warning text-center rounded-circle"
+                onclick="clickOperation('x')"
+              >
+                x
+              </div>
+            </div>
+
+            <div className="col-3">
+              <div
+                className="button
+                            fs-5 fw-bold text-bg-dark text-center rounded-circle"
+                onclick="clickNumber(4)"
+              >
+                4
+              </div>
+            </div>
+            <div className="col-3">
+              <div
+                className="button
+                            fs-5 fw-bold text-bg-dark text-center rounded-circle"
+                onclick="clickNumber(5)"
+              >
+                5
+              </div>
+            </div>
+            <div className="col-3">
+              <div
+                className="button
+                            fs-5 fw-bold text-bg-dark text-center rounded-circle"
+                onclick="clickNumber(6)"
+              >
+                6
+              </div>
+            </div>
+            <div className="col-3">
+              <div
+                className="button
+                            fs-5 fw-bold text-bg-warning text-center rounded-circle"
+                onclick="clickOperation('-')"
+              >
+                -
+              </div>
+            </div>
+
+            <div className="col-3">
+              <div
+                className="button
+                            fs-5 fw-bold text-bg-dark text-center rounded-circle"
+                onclick="clickNumber(1)"
+              >
+                1
+              </div>
+            </div>
+            <div className="col-3">
+              <div
+                className="button
+                            fs-5 fw-bold text-bg-dark text-center rounded-circle"
+                onclick="clickNumber(2)"
+              >
+                2
+              </div>
+            </div>
+            <div className="col-3">
+              <div
+                className="button
+                            fs-5 fw-bold text-bg-dark text-center rounded-circle"
+                onclick="clickNumber(3)"
+              >
+                3
+              </div>
+            </div>
+            <div className="col-3">
+              <div
+                className="button
+                            fs-5 fw-bold text-bg-warning text-center rounded-circle"
+                onclick="clickOperation('+')"
+              >
+                +
+              </div>
+            </div>
+
+            <div className="col-6">
+              <div
+                className="button
+                            fs-5 fw-bold text-bg-dark text-center rounded-pill"
+                onclick="clickNumber(0)"
+              >
+                0
+              </div>
+            </div>
+            <div className="col-3">
+              <div
+                className="button
+                            fs-5 fw-bold text-bg-dark text-center rounded-circle"
+                onclick="clickNumber('.')"
+              >
+                .
+              </div>
+            </div>
+            <div className="col-3">
+              <div
+                className="button
+                            fs-5 fw-bold text-bg-warning text-center rounded-circle"
+                onclick="calculate()"
+              >
+                =
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
